f103a4ba94136c46eed46f49a5db8deb1022c102
Ross Andrews <randrews@kallisti.local>
2009-11-27 22:24:32 -0600

    Requiring authlogic in app controller to make it work on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+require 'authlogic'
+
 class ApplicationController < ActionController::Base
+
   helper :all # include all helpers, all the time
 
   before_filter :login_session
